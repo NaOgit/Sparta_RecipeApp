dd63ded0ceff8aaa0b73b5eb805990111534bcf3
NaO <noukongka@spartaglobal.com>
2018-08-30 21:53:28 +0100

    Initial setup

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'recipes#index'
+  # get 'recipes/index'
+  # get 'recipes/show'
+  # get 'recipes/create'
+  # get 'recipes/new'
+  # get 'recipes/edit'
+  # get 'recipes/update'
+  # get 'recipes/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
